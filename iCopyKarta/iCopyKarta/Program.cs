4ee5ed41bfdd0861c46401b6dacf8d2ff4e8615e
Cassim Vanker <cassimv@dut.ac.za>
2023-02-05 15:45:05 +0200

    Updated Output To the Program.cs

--- iCopyKarta/iCopyKarta/Program.cs
+++ iCopyKarta/iCopyKarta/Program.cs
@@ -1,2 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Character Copy Kata");
+Console.WriteLine("Run the test cases !");
